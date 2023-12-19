6db40ceca82ceca76304eacd08e0804b9a004b0a
bagas_sfrzn <safrizanbagas@gmail.com>
2023-12-19 17:43:34 +0700

    nambah modul library

---
+++ tilabs-library/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Tilabs Library',
+    'version': '1.0',
+    'description': 'Tilabs Library is modul for library',
+    'summary': 'Tilabs library by bagas',
+    'author': 'Bagas_sfrzn',
+    'website': 'bagas.id',
+    'license': 'LGPL-3',
+    'category': 'Knowledge',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        ''
+    ],
+    'demo': [
+        ''
+    ],
+    'auto_install': False,
+    'application': True,
+    'assets': {
+        
+    }
+}
